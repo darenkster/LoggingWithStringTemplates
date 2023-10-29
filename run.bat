ae524c74afa27bed5c8e7bf984eb6cf3a8e15e2f
darenkster <renkosp@gmail.com>
2023-10-29 12:59:36 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,6 @@
 @ECHO OFF
 
-%JAVA_HOME%\bin\java.exe --enable-preview -classpath "lib\slf4j-api-2.0.9.jar;lib\logback-classic-1.4.11.jar;lib\logback-core-1.4.11.jar;bin\." de.darenkster.stringtemplates2slf4j.Main
+%JAVA_HOME%\bin\java.exe ^
+--enable-preview 
+^-classpath "lib\slf4j-api-2.0.9.jar;lib\logback-classic-1.4.11.jar;lib\logback-core-1.4.11.jar;bin\." 
+^de.darenkster.stringtemplates2slf4j.Main
