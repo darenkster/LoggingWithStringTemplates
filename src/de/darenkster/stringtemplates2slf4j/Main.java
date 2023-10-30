2977e6deacebcdbc569257ab6534196da29a65e8
darenkster <renkosp@gmail.com>
2023-10-30 11:00:26 +0100

    refactor and call log method with throwable if present

--- src/de/darenkster/stringtemplates2slf4j/Main.java
+++ src/de/darenkster/stringtemplates2slf4j/Main.java
@@ -6,10 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
         var test = "test";
+        var ex = new Exception();
         INFO."This is a info \{test}";
-        ERROR."This is a error \{test}";
+        ERROR."This is a error \{test} \{ex}";
         DEBUG."This is a debug \{test}";
-        WARN."This is a warn \{test}";
+        WARN."""
+                This is a warn 
+                \{test}""";
         TRACE."This is a trace \{test}";
         
         new Object() {
@@ -23,11 +26,14 @@ public class Main {
     }
     /*
     * Prints outs:
-    * 09:15:33.309 [main] INFO de.darenkster.stringtemplates2slf4j.Main -- This is a info test
-    * 09:15:33.311 [main] ERROR de.darenkster.stringtemplates2slf4j.Main -- This is a error test
-    * 09:15:33.312 [main] DEBUG de.darenkster.stringtemplates2slf4j.Main -- This is a debug test
-    * 09:15:33.312 [main] WARN de.darenkster.stringtemplates2slf4j.Main -- This is a warn test
-    * 09:15:33.313 [main] INFO de.darenkster.stringtemplates2slf4j.Main -- This is a info test from within a anonymous class
-    * 09:15:33.314 [main] INFO de.darenkster.stringtemplates2slf4j.Main -- This is a info test from within a lambda
-    */
+    * 10:33:44.899 [main] INFO de.darenkster.stringtemplates2slf4j.Main -- This is a info test
+    * 10:33:44.907 [main] ERROR de.darenkster.stringtemplates2slf4j.Main -- This is a error test java.lang.Exception
+    * java.lang.Exception: null
+    *   at de.darenkster.stringtemplates2slf4j.Main.main(Main.java:9)
+    * 10:33:44.909 [main] DEBUG de.darenkster.stringtemplates2slf4j.Main -- This is a debug test
+    * 10:33:44.910 [main] WARN de.darenkster.stringtemplates2slf4j.Main -- This is a warn
+    * test
+    * 10:33:44.912 [main] INFO de.darenkster.stringtemplates2slf4j.Main -- This is a info test from within an anonymous class
+    * 10:33:44.912 [main] INFO de.darenkster.stringtemplates2slf4j.Main -- This is a info test from within a lambda
+    */ 
 }
--- src/de/darenkster/stringtemplates2slf4j/loggers/LOG.java
+++ src/de/darenkster/stringtemplates2slf4j/loggers/LOG.java
@@ -2,7 +2,9 @@ package de.darenkster.stringtemplates2slf4j.loggers;
 
 import java.lang.StackWalker.StackFrame;
 import java.lang.StringTemplate.Processor;
-import java.util.*;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,60 +16,86 @@ import org.slf4j.LoggerFactory;
 * INFO."Logtext"
 */
 public class LOG{
-	/* The Logger Fields*/
-	public static final Processor<Void, RuntimeException> INFO;
-	public static final Processor<Void, RuntimeException> ERROR;
-	public static final Processor<Void, RuntimeException> DEBUG;
-	public static final Processor<Void, RuntimeException> WARN;
-	public static final Processor<Void, RuntimeException> TRACE;
-	
-	static {
-		INFO = StringTemplate.Processor.of((StringTemplate st) -> {
-			Logger logger = getLoggerForDeclaringClass();
-			String interpolatedMessage = st.interpolate();
-			logger.info(interpolatedMessage);
-			return null;
-		});
-		ERROR = StringTemplate.Processor.of((StringTemplate st) -> {
-			Logger logger = getLoggerForDeclaringClass();
-			String interpolatedMessage = st.interpolate();
-			logger.error(interpolatedMessage);
-			return null;
-		});
-		DEBUG = StringTemplate.Processor.of((StringTemplate st) -> {
-			Logger logger = getLoggerForDeclaringClass();
-			String interpolatedMessage = st.interpolate();
-			logger.debug(interpolatedMessage);
-			return null;
-		});
-		WARN = StringTemplate.Processor.of((StringTemplate st) -> {
-			Logger logger = getLoggerForDeclaringClass();
-			String interpolatedMessage = st.interpolate();
-			logger.warn(interpolatedMessage);
-			return null;
-		});
-		TRACE = StringTemplate.Processor.of((StringTemplate st) -> {
-			Logger logger = getLoggerForDeclaringClass();
-			String interpolatedMessage = st.interpolate();
-			logger.trace(interpolatedMessage);
-			return null;
-		});
-	}
-	
-	/** Looking up what class is calling the Logger */
-	public static final Logger getLoggerForDeclaringClass() {
-		List<StackFrame> stackTrace = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE).walk((sfs) -> {
-			return sfs
-					// TODO more filters for classes with weird names
-					.filter((StackFrame sf) -> !sf.getDeclaringClass().isAnonymousClass())
-					.filter((StackFrame sf) -> !sf.getDeclaringClass().equals(LOG.class))
-					.findFirst()
-					.stream()
-					.toList();						
-					
-			});
-		
-			Class<?> clazz = stackTrace.get(0).getDeclaringClass();
-			return LoggerFactory.getLogger(clazz);
-	}
+    /* The Logger Fields*/
+    public static final Processor<Void, RuntimeException> INFO;
+    public static final Processor<Void, RuntimeException> ERROR;
+    public static final Processor<Void, RuntimeException> DEBUG;
+    public static final Processor<Void, RuntimeException> WARN;
+    public static final Processor<Void, RuntimeException> TRACE;
+    
+    static {
+        INFO = StringTemplate.Processor.of((StringTemplate stringTemplate) -> {
+            Logger logger = getLoggerForCallingClass();
+            interpolateAndLog(stringTemplate, 
+                (message,throwable) -> logger.info(message, throwable),
+                (message) -> logger.info(message) 
+                );
+            return null;
+        });
+        ERROR = StringTemplate.Processor.of((StringTemplate stringTemplate) -> {
+            Logger logger = getLoggerForCallingClass();
+            interpolateAndLog(stringTemplate, 
+                (message,throwable) -> logger.error(message, throwable),
+                (message) -> logger.error(message) 
+                );
+            return null;
+        });
+        DEBUG = StringTemplate.Processor.of((StringTemplate stringTemplate) -> {
+            Logger logger = getLoggerForCallingClass();
+            interpolateAndLog(stringTemplate, 
+                (message,throwable) -> logger.debug(message, throwable),
+                (message) -> logger.debug(message) 
+                );
+            return null;
+        });
+        WARN = StringTemplate.Processor.of((StringTemplate stringTemplate) -> {
+            Logger logger = getLoggerForCallingClass();
+            interpolateAndLog(stringTemplate, 
+                (message,throwable) -> logger.warn(message, throwable),
+                (message) -> logger.warn(message) 
+                );
+            return null;
+        });
+        TRACE = StringTemplate.Processor.of((StringTemplate stringTemplate) -> {
+            Logger logger = getLoggerForCallingClass();
+            interpolateAndLog(stringTemplate, 
+                (message,throwable) -> logger.trace(message, throwable),
+                (message) -> logger.trace(message) 
+                );
+            return null;
+        });
+    }
+    
+    /** Looking up what class is calling the Logger */
+    private static final Logger getLoggerForCallingClass() {
+        List<StackFrame> stackTrace = StackWalker
+            .getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE)
+            .walk((stackFrameStream) -> 
+                stackFrameStream
+                    // TODO more filters for classes with weird names
+                    .filter((stackFrame) -> !stackFrame.getDeclaringClass().isAnonymousClass())
+                    .filter((stackFrame) -> !stackFrame.getDeclaringClass().equals(LOG.class))
+                    .findFirst()
+                    .stream()
+                    .toList());
+        
+        Class<?> clazz = stackTrace.getFirst().getDeclaringClass();
+        return LoggerFactory.getLogger(clazz);
+    }
+
+    /** Interpolate the String and either call the basic log method or the one with the throwable */
+    private static void interpolateAndLog(StringTemplate stringTemplate, 
+            BiConsumer<String, Throwable> exceptionLogMethod,
+            Consumer<String> basicLogMethod) {
+        String interpolatedMessage = stringTemplate.interpolate();
+        stringTemplate.values()
+            .stream()
+            .filter(Throwable.class::isInstance)
+            .map(Throwable.class::cast)
+            .findFirst()
+            .ifPresentOrElse(
+                throwable -> exceptionLogMethod.accept(interpolatedMessage, throwable), 
+                () ->basicLogMethod.accept(interpolatedMessage));
+        
+    }
 }
